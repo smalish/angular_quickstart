bbf9fd2c3bd3edea14ba5452a57852e9344a2b4d
yangying <1349365218@qq.com>
2016-11-07 18:47:46 +0800

    commit project

--- GitHub/angular/angularjs/test1/js/app.js
+++
@@ -1,5 +0,0 @@
-var loginApp = angular.module('login', []);
-
-loginApp.config(function() {
-   
-});
---
+++ angular_quickstart/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular App --- yangying</h1>'
+})
+export class AppComponent { }
