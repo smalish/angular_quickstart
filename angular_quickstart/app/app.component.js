0422a2572569e4d22dcba6c273a5f635f352656e
yangying <1349365218@qq.com>
2016-11-08 14:06:11 +0800

    hero project: o Please enter the commit message for your changes. Lines starting

--- angular_quickstart/app/app.component.js
+++ angular_quickstart/app/app.component.js
@@ -9,13 +9,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular App --- yangying</h1>'
+            template: "\n  \t\t<h1>{{title}}</h1>\n  \t\t<h2>{{hero.name}} details!</h2>\n  \t\t<div><label>id: </label>{{hero.id}}</div>\n  \t\t<div>\n  \t\t\t<label>name: </label>\n  \t\t\t<input value=\"{{hero.name}}\" placeholder=\"name\">\n  \t\t\t\n  \t\t</div>\n  \t\t"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
