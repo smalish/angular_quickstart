5a9066f5dcfea0499b523f51ae68e79c79faf593
yangying <1349365218@qq.com>
2016-11-10 19:43:40 +0800

    重构路由

--- angular_quickstart/app/dashboard.component.ts
+++ angular_quickstart/app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }   from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -12,11 +13,24 @@ import { HeroService } from './hero.service';
 
 export class DashboardComponent {
 	heroes: Hero[] = [];
+	selectedHero : Hero;
 
-  	constructor(private heroService: HeroService) { }
+  	constructor(
+  		private router: Router,
+  		private heroService: HeroService
+  	) { }
 
   	ngOnInit(): void {
     	this.heroService.getHeroes()
       	.then(heroes => this.heroes = heroes.slice(1, 5));
   	}
+
+  	onSelect(hero : Hero):void{
+  		this.selectedHero = hero;
+  	}
+
+  	gotoDetail(): void{
+  		this.router.navigate(['/detail', this.selectedHero.id]);
+  	}
+
  }
--- angular_quickstart/app/heroes.component.js
+++ angular_quickstart/app/heroes.component.js
@@ -11,7 +11,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var hero_service_1 = require('./hero.service');
 var HeroesComponent = (function () {
-    function HeroesComponent(heroService) {
+    function HeroesComponent(router, heroService) {
+        this.router = router;
         this.heroService = heroService;
     }
     HeroesComponent.prototype.getHeroes = function () {
@@ -26,11 +27,12 @@ var HeroesComponent = (function () {
     };
     HeroesComponent = __decorate([
         core_1.Component({
+            moduleId: module.id,
             selector: 'my-heroes',
-            template: "\n  \t\t\n\n  \t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\">\n\t\t  \t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t  \t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t</li>\n\t\t</ul>\n\n  \t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  \t\t",
-            styles: ["\n\t\t  .selected {\n\t\t    background-color: #CFD8DC !important;\n\t\t    color: white;\n\t\t  }\n\t\t  .heroes {\n\t\t    margin: 0 0 2em 0;\n\t\t    list-style-type: none;\n\t\t    padding: 0;\n\t\t    width: 15em;\n\t\t  }\n\t\t  .heroes li {\n\t\t    cursor: pointer;\n\t\t    position: relative;\n\t\t    left: 0;\n\t\t    background-color: #EEE;\n\t\t    margin: .5em;\n\t\t    padding: .3em 0;\n\t\t    height: 1.6em;\n\t\t    border-radius: 4px;\n\t\t  }\n\t\t  .heroes li.selected:hover {\n\t\t    background-color: #BBD8DC !important;\n\t\t    color: white;\n\t\t  }\n\t\t  .heroes li:hover {\n\t\t    color: #607D8B;\n\t\t    background-color: #DDD;\n\t\t    left: .1em;\n\t\t  }\n\t\t  .heroes .text {\n\t\t    position: relative;\n\t\t    top: -3px;\n\t\t  }\n\t\t  .heroes .badge {\n\t\t    display: inline-block;\n\t\t    font-size: small;\n\t\t    color: white;\n\t\t    padding: 0.8em 0.7em 0 0.7em;\n\t\t    background-color: #607D8B;\n\t\t    line-height: 1em;\n\t\t    position: relative;\n\t\t    left: -1px;\n\t\t    top: -4px;\n\t\t    height: 1.8em;\n\t\t    margin-right: .8em;\n\t\t    border-radius: 4px 0 0 4px;\n\t\t  }\n\t\t"]
+            templateUrl: 'heroes.component.html',
+            styleUrls: ['heroes.component.css']
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService])
+        __metadata('design:paramtypes', [Array, hero_service_1.HeroService])
     ], HeroesComponent);
     return HeroesComponent;
 }());
